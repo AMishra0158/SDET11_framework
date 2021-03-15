6551cd0f087ecbc24cfceed9d7301e2bdc17c8ff
ASUS <ASUS@Amit>
2021-03-15 13:38:43 +0530

    contact page created

---
+++ practiseMaven/src/test/java/com/crm/comcast/MavenTest/ContactTest.java
@@ -0,0 +1,10 @@
+package com.crm.comcast.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void contact() {
+		System.out.println("contactsmain page");
+	}
+}
