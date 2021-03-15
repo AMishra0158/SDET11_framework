337e9026b3e20fa644ce76963808fa8c53ea0ceb
ASUS <ASUS@Amit>
2021-03-15 13:55:23 +0530

    NewAccount

---
+++ practiseMaven/src/test/java/com/crm/comcast/MavenTest/NewAccountTest.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class NewAccountTest {
+	@Test
+	public void NewAccount() {
+		System.out.println("new acount created");
+	}
+
+}
