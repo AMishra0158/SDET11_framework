c5ac3e4edeffd48767a13c50fdde164af0f38c0e
ASUS <ASUS@Amit>
2021-03-15 12:56:50 +0530

    contact method is added

---
+++ practiseMaven/src/test/java/com/crm/comcast/MavenTest/Maven1Test.java
@@ -0,0 +1,24 @@
+package com.crm.comcast.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class Maven1Test {
+
+	@Test
+	public void createContact()
+
+	{
+		System.out.println("hello Maven");
+	}
+	@Test
+	public void modifyContact()
+	{
+		System.out.println("modify contact");
+	}
+	@Test
+	public void Maven3() {
+		System.out.println("bye Maven");
+	}
+}
+
+//git repository url---(https://github.com/AMishra0158/SDET11_framework.git)
